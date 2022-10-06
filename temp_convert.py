2d826cdb951d0af59a669881c3400e6a75b36b47
Niima23 <114267273+Niima23@users.noreply.github.com>
2022-10-05 23:24:11 -0700

    Delete temp_convert.py

--- temp_convert.py
+++
@@ -1,7 +0,0 @@
-#python program to convert temperature in celsius
-C=float(input("Enter a temperature in celsius: "))
-#convert to Fahrenheit
-F=(9/5)*C+32
-#print the Temperature in farhrenheit
-print("Temperature in Fahrenheit is ",F)
-
